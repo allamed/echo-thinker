09ea25af5aec3f99b3b3fc004ad890e1953fc454
unknown <allamedd37@gmail.com>
2023-11-12 03:17:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Recorder } from 'react-voice-recorder';
+import 'react-voice-recorder/dist/index.css'
+import "./App.css";
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [audioDetails, setAudioDetails] = useState({
+    url: null,
+    blob: null,
+    chunks: null,
+    duration: { h: 0, m: 0, s: 0 }
+  });
+  const [transcription, setTranscription] = useState('');
+
+
+  const handleAudioStop = (data) => {
+    console.log(data);
+    setAudioDetails(data);
+    console.log(data)
+  };
+const handleCountDown=(data)=>{
+
 }
+    const handleAudioUpload = async () => {
+        if (audioDetails.blob) {
+            const formData = new FormData();
+            formData.append('audio', audioDetails.blob, 'audio.webm');
+
+            try {
+                const response = await axios.post('http://localhost:3000/transcribe', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+                setTranscription(response.data.transcription);
+                console.log("transcription result", transcription)
+            } catch (error) {
+                console.error('Error uploading the audio file:', error);
+            }
+        } else {
+            console.error('No audio to upload');
+        }
+    };
+
+
+  const handleReset = () => {
+    setAudioDetails({
+      url: null,
+      blob: null,
+      chunks: null,
+      duration: { h: 0, m: 0, s: 0 }
+    });
+  };
+
+  return (
+      <div style={{display:"flex", flexDirection:"column"}}>
+            <textarea
+                style={{height:80, flexGrow:1}}
+                className="text-area"
+                placeholder="Voice-to-text output will appear here..."
+                value={transcription}
+           / >
+
+
+        <div style={{height:"10%"}} >
+          <Recorder
+              record={true}
+              title={"New recording"}
+              audioURL={audioDetails.url}
+              showUIAudio
+              handleAudioStop={data => handleAudioStop(data)}
+              handleAudioUpload={data => handleAudioUpload(data)}
+              handleCountDown={data => handleCountDown(data)}
+              handleReset={() => handleReset()}
+              mimeTypeToUseWhenRecording={"audio/webm"}
+          />
+        </div>
+      </div>
+  );
+};
 
 export default App;
